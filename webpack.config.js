dd830bbca8df40d25c24c264d486ac03429c4d7e
Nikita Kuvykin <arrayofchar@inbox.ru>
2019-01-15 12:20:51 +0500

    - add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dev/js/'),
     filename: 'script.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
